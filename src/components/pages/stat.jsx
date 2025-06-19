e0bce95d7f66e6645840c14167d045a95c33d1cf
эдуард антипов <e.antipov@automacon.ru>
2025-06-19 14:35:07 +0300

    test

--- src/components/pages/stat.jsx
+++ src/components/pages/stat.jsx
@@ -810,7 +810,7 @@ const Stat = (props) => {
                                 label="Навоз бесподстилочный"
                             />
                             {checkboxStates.navoz2 &&
-                                <div style={{ flex: 1 }}>
+                                <div style={{ flex: 1, width: '100%' }}>
                                     <InputComponent inputValue={navoz2Value} action={setNavoz2Value} placeholder={"Введите внесенное количество навоза бесподстилочного за год в тоннах"} />
                                 </div>}
                         </div>
